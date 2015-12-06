779c2dbf1f0f1700d19ac7120f57033b9fa558e6
TokmakidisP <pantok3@gmail.com>
2015-12-06 12:15:57 +0200

    renames

--- voting/models.py
+++ voting/models.py
@@ -4,11 +4,11 @@ from django.db import models
 from django.utils import timezone
 
 
-class Vote_main(models.Model):
+class VoteMain(models.Model):
     username = models.CharField(max_length=200)
     area = models.IntegerField(default=0)
-    voteProxy = models.IntegerField(default=0)
-    pubDate = models.DateTimeField('date published')
+    vote_proxy = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('date published')
     def __unicode__(self):              # __unicode__ on Python 2
         return self.question_text
     def was_published_recently(self):
@@ -16,25 +16,25 @@ class Vote_main(models.Model):
 		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 		
-class Vote_result(models.Model):
-    voteMain = models.OneToOneField(Vote_main)
+class VoteResult(models.Model):
+    vote_main = models.OneToOneField(Vote_main)
     votes = models.IntegerField(default=0)
     def __unicode__(self):              # __unicode__ on Python 2
         return self.votes
         
 
-class Vote_texts(models.Model):
-    voteM = models.OneToOneField(Vote_main)
-    voteName = models.CharField(max_length=200)
-    voteDescription = models.CharField
+class VoteTexts(models.Model):
+    vote_m = models.OneToOneField(Vote_main)
+    vote_name = models.CharField(max_length=200)
+    vote_description = models.CharField
     def __unicode__(self):              # __unicode__ on Python 2
         return self.vote_name
 
 
 class UserExtra(models.Model):
 	user = models.OneToOneField(User)
-	areaOfResidence = models.CharField(max_length=100)
-	userID = models.IntegerField
+	area_of_residence = models.CharField(max_length=100)
+	user_ID = models.IntegerField
 	def __unicode__(self):
         return self.user.username
 	
