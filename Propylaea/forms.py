a20f1e326b44f71efe5db063245efdcbefb73247
TokmakidisP <pantok3@gmail.com>
2016-02-06 21:28:56 +0200

    improved form names

--- Propylaea/forms.py
+++ Propylaea/forms.py
@@ -5,5 +5,3 @@ class SignUp(forms.Form):
     Name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name', 'required': True}))
     PhoneNumber = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number', 'required': False}))
     Password = forms.CharField(max_length=32, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password', 'required': True}))
-
-
