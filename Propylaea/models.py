bb4038c8ab2d1aeaa55e57a0325b5dfb9a82c918
TokmakidisP <pantok3@gmail.com>
2016-02-07 10:07:24 +0200

    Added UserExtra model

--- Propylaea/models.py
+++ Propylaea/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserExtra(models.Model):
+	# Links UserExtra to User model instance.
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	
+	#Extra attributes added to user
+	phoneNumber = models.CharField(max_length=30, blank=True)
