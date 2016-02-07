b8bed17ed66fcb636768ddeb37fe6d5ed2d63795
TokmakidisP <pantok3@gmail.com>
2016-02-07 10:11:33 +0200

    Changed Propylaea forms to correctly sign up a user

--- Propylaea/forms.py
+++ Propylaea/forms.py
@@ -1,9 +1,23 @@
 from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from Propylaea.models import UserExtra
 
-class SignUp(forms.Form):
-    Email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email', 'required': True}))
-    Name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name', 'required': True}))
-    PhoneNumber = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number', 'required': False}))
-    Password = forms.CharField(max_length=32, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password', 'required': True}))
+class UserFormExtra(ModelForm):   
+    class Meta:
+        model = UserExtra
+        fields = ('phoneNumber',)
+        widgets = {
+			'phoneNumber' : forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number', 'required': False}),
+        }
 
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        widgets = {
+			'username' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name', 'required': True}),
+			'email' : forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email', 'required': True}),
+			'password' : forms.PasswordInput(attrs={'max_length': '32', 'class': 'form-control', 'placeholder': 'Password', 'required': True}),
+        }
 
