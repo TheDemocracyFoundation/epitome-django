12afc69bacd8910a3f45c03e75c57c310ebdc17f
GMavropalias <georgios.mavropalias@gmail.com>
2016-06-09 22:55:49 +0800

    clear branch

--- Propylaea/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from Propylaea import views
-
-urlpatterns = [
-    #~ url(r'^$', views.UserIndex, name='index'),
-    url(r'^signup', views.SignUpV, name='signup'),
-    url(r'^login', views.LogIn, name='login')
-]
