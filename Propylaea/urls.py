5472be14f797aedf8ee50df005076e5a80f690ca
TokmakidisP <pantok3@gmail.com>
2016-09-11 21:48:39 +0300

    Added logout url

--- Propylaea/urls.py
+++ Propylaea/urls.py
@@ -4,5 +4,6 @@ from Propylaea import views
 urlpatterns = [
     #~ url(r'^$', views.UserIndex, name='index'),
     url(r'^signup', views.SignUpV, name='signup'),
-    url(r'^login', views.LogIn, name='login')
+    url(r'^login', views.LogIn, name='login'),
+    url(r'^logout', views.UsrLogout, name='logout'),
 ]
