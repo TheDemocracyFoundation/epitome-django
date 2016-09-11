f4d0a5308dc31dd96f63d98d74e0e33837efd233
TokmakidisP <pantok3@gmail.com>
2016-09-11 21:49:57 +0300

    Modified views.py to work with Propylaea templates

--- Propylaea/views.py
+++ Propylaea/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
-from django.contrib.auth import authenticate, login
-from Propylaea.forms import UserForm, UserFormExtra
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from Propylaea.forms import LoginForm, SignUpForm
+from django.template import loader
+from django.contrib.auth.decorators import login_required
 
 def SignUpV(request):
 	# Signed up flag
@@ -10,45 +12,69 @@ def SignUpV(request):
     # If the request is type of POST then proccess data
 	if request.method == 'POST':
         # Create 2 form instances and populate them with data from the POST request
-		form1 = UserForm(request.POST)
-		form2 = UserFormExtra(request.POST)
+		form1 = SignUpForm(request.POST)
         # Check whether it's valid
-		if form1.is_valid() and form2.is_valid():
+		if form1.is_valid():
 			# Save main user form first and hash password
 			try:
 				user = form1.save(commit=False)
 				user.set_password(user.password)
 				user.save()
-				# Save second form, where its id equals the user's
-				profile = form2.save(commit=False)
-				profile.user = user
-				profile.save()
 				# Change sign up flag to true
 				signedUp = True
+				return HttpResponse("Your account is registered.")
 			except:
 				signedUp = False
+		else:
+			return HttpResponse("Your account is not registered.")
     # If request is not POST create empty forms
 	else:
-		form1 = UserForm()
-		form2 = UserFormExtra()
-
-	return render(request, 'signup.html', {'SignUpForm': form1, 'SignUpFormEx': form2})
+		#form1 = SignUpForm()
+		template = loader.get_template('Propylaea/login_register.html')
+		context = {
+			'SignUpForm': SignUpForm,
+			'LoginForm': LoginForm,
+		}
+		return HttpResponse(template.render(context, request))
+		#return HttpResponseRedirect('/Eisegesis/Eisegesis')
+		#return render(request, 'Propylaea/login_register.html', {'SignUpForm': SignUpForm, 'LoginForm': LoginForm})
 
 def LogIn(request):
 	# If the request is type of POST then proccess data
 	if request.method == 'POST':
-		email = request.POST.get('email')
+		#forml = LoginForm(request.POST)
+		#if forml.is_valid():
+		#email = request.POST.get('email')
+		username = request.POST.get('username')
 		password = request.POST.get('password')
+		#email = request.POST['email']
+		#password = request.POST['password']
 		# Authenticate
-		user = authenticate(email=email, password=password)
+		#user = authenticate(email=email, password=password)
+		user = authenticate(username=username, password=password)
 		# User valid
 		if user is not None:
 			if user.is_active:
 				login(request, user)
-				return HttpResponse("Your account is loged in.")
+				#return HttpResponse("Your account is logged in.")
+				return HttpResponseRedirect('/eisegesis/')
 			else:
 				return HttpResponse("Your account is disabled.")
 		else:
-			return HttpResponse("Invalid login details supplied.")
+			return HttpResponse("Invalid login details supplied")#: {0}, {1}".format(email, password))
 	else:
-		return render(request, 'login.html', {})
+		template = loader.get_template('Propylaea/login_register.html')
+		context = {
+			'LoginForm': LoginForm,
+			'SignUpForm': SignUpForm,
+		}
+		return HttpResponse(template.render(context, request))
+		#return render_to_response('Propylaea/login_register.html', {}, context)
+
+@login_required
+def UsrLogout(request):
+	# Since we know the user is logged in, we can now just log them out.
+	logout(request)
+
+	# Take the user back to the homepage.
+	return HttpResponseRedirect('/user/login/')
