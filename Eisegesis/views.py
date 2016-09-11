0c51e41b5065945c8cbd168a569c30113213e543
TokmakidisP <pantok3@gmail.com>
2016-09-11 21:40:20 +0300

    Modified views to work with templates

--- Eisegesis/views.py
+++ Eisegesis/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Proposals,ProposalChoice
+
+@login_required
+def index(request):
+	latest_proposal_list = Proposals.objects.order_by('-P_CREATION')#[:5]
+	template = loader.get_template('Eisegesis/index.html')
+	context = {
+		'latest_proposal_list': latest_proposal_list,
+	}
+	return HttpResponse(template.render(context, request))
+	
+	
+@login_required
+def moreInfo(request, proposals_id):
+	proposal = get_object_or_404(Proposals, pk=proposals_id)
+	#PChoice = get_object_or_404(ProposalChoice)
+	template = loader.get_template('Eisegesis/propDetail.html')
+	context = {
+		'proposal': proposal,
+		#'PChoice': PChoice,
+	}
+	#return render(request, 'polls/detail.html', {'question': question})
+	return HttpResponse(template.render(context, request))
+
+#def index(request):
+#	return HttpResponse("Hello")
+
