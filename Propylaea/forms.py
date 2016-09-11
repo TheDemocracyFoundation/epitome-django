b8ee20484ddc45c6f71a72551062a4a26390795e
TokmakidisP <pantok3@gmail.com>
2016-09-11 21:46:17 +0300

    Modified forms.py to work with login and register template

--- Propylaea/forms.py
+++ Propylaea/forms.py
@@ -1,23 +1,25 @@
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.models import User
-from Propylaea.models import UserExtra
 
-class UserFormExtra(ModelForm):   
+
+
+class LoginForm(ModelForm):
     class Meta:
-        model = UserExtra
-        fields = ('phoneNumber',)
+        model = User
+        fields = ('username', 'password')
         widgets = {
-			'phoneNumber' : forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number', 'required': False}),
+			#'email' : forms.EmailInput(attrs={'name': 'Email', 'required': True}),
+			'username' : forms.TextInput(attrs={'name': 'name', 'required': True}),
+			'password' : forms.PasswordInput(attrs={'max_length': '64', 'name': 'password', 'required': True}),
         }
-
-class UserForm(ModelForm):
+        
+class SignUpForm(ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
         widgets = {
-			'username' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name', 'required': True}),
-			'email' : forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email', 'required': True}),
-			'password' : forms.PasswordInput(attrs={'max_length': '32', 'class': 'form-control', 'placeholder': 'Password', 'required': True}),
+			'username' : forms.TextInput(attrs={'name': 'name', 'required': True}),
+			'email' : forms.EmailInput(attrs={'name': 'email', 'required': True}),
+			'password' : forms.PasswordInput(attrs={'name': 'password', 'required': True}),
         }
-
