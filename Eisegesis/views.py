40df1017472cff79646f5629091691c220af056b
TokmakidisP <pantok3@gmail.com>
2016-11-27 17:59:17 +0200

    Added form and radio buttons

--- Eisegesis/views.py
+++ Eisegesis/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 from .models import Proposals,ProposalChoice
 
@@ -13,8 +14,8 @@ def index(request):
 		'latest_proposal_list': latest_proposal_list,
 	}
 	return HttpResponse(template.render(context, request))
-	
-	
+
+
 @login_required
 def moreInfo(request, proposals_id):
 	proposal = get_object_or_404(Proposals, pk=proposals_id)
@@ -26,6 +27,26 @@ def moreInfo(request, proposals_id):
 	}
 	#return render(request, 'polls/detail.html', {'question': question})
 	return HttpResponse(template.render(context, request))
+
+
+def vote(request, proposals_id):
+	proposal = get_object_or_404(Proposals, pk=proposals_id)
+	try:
+		selected_choice = proposal.PChoice.get(pk=request.POST['choice'])
+	except (KeyError, ProposalChoice.DoesNotExist):						#probably wrong
+		# Redisplay the question voting form.
+		return render(request, 'Eisegesis/propDetail.html', {
+			#'question': question,
+			'proposal': proposal,
+			'error_message': "You didn't select a choice.",
+		})
+	else:
+		selected_choice.PCHOICE_VOTES += 1
+		selected_choice.save()
+		# Always return an HttpResponseRedirect after successfully dealing
+		# with POST data. This prevents data from being posted twice if a
+		# user hits the Back button.
+		return HttpResponseRedirect(reverse('Eisegesis:index'))#, args=(proposal.id,)))
 
 #def index(request):
 #	return HttpResponse("Hello")
