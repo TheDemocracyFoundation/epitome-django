b2c1985d89a9cc7cfbc2354ba1eb184e0fc39e8b
Georgios Mavropalias <MavropaliasG@users.noreply.github.com>
2017-04-04 08:49:02 +0800

    Update settings.py

--- Epitome/settings.py
+++ Epitome/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'Epitome.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Atlas',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
