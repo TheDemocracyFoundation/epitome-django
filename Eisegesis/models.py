12794bafb13fabecee49fd81058f343dc91329be
panos panos <pantok3@gmail.com>
2017-11-12 16:23:49 +0200

    Renamed to polls

--- Eisegesis/models.py
+++ Eisegesis/models.py
@@ -7,31 +7,31 @@ from django.contrib.auth.models import User, Group
 
 
 class PollCat(models.Model):
-	PCAT_CAT = models.CharField(max_length=100)                            #the category of the poll
+	PCAT_CAT = models.CharField(max_length=100)                            # the category of the poll
 	def __unicode__(self):
 		return self.PCAT_CAT
 
 
 class Polls(models.Model):
-	P_CODE = models.CharField(max_length=200)                              #the code of the poll
-	P_TITLE = models.CharField(max_length=100)                             #the title of the poll
-	P_SHRBODY = models.CharField(max_length=300)                           #the short body of the poll
-	P_BODY = models.CharField(max_length=1000)                             #the body (main text) of the poll
-	P_CREATION = models.DateTimeField(default = timezone.now())            #the creation date and time of the poll
-	P_STARTDT = models.DateTimeField()                                     #the starting date and time of the poll
-	P_ENDDT = models.DateTimeField()                                       #the ending date and time of the poll
-	P_DURATION = models.IntegerField(default=0)                            #the duration of the poll
-	P_CODE2 = models.CharField(max_length=200, blank = True)               #the code2 of the poll (protocol number, approval number)
-	USER_CREATORID = models.ForeignKey(User, on_delete=models.CASCADE)     #the user (admin) who created the poll
-	UGRP_GROUPID = models.ForeignKey(Group, on_delete=models.CASCADE)      #the group of the user that created the poll
-	PCAT_CAT = models.ForeignKey(PollCat)                              #the category of the poll
-	#PCHOICE_CHOICE = models.ForeignKey(PollChoice)                    #the voting choices of the poll
+	P_CODE = models.CharField(max_length=200)                              # the code of the poll
+	P_TITLE = models.CharField(max_length=100)                             # the title of the poll
+	P_SHRBODY = models.CharField(max_length=300)                           # the short body of the poll
+	P_BODY = models.CharField(max_length=1000)                             # the body (main text) of the poll
+	P_CREATION = models.DateTimeField(default = timezone.now())            # the creation date and time of the poll
+	P_STARTDT = models.DateTimeField()                                     # the starting date and time of the poll
+	P_ENDDT = models.DateTimeField()                                       # the ending date and time of the poll
+	P_DURATION = models.IntegerField(default=0)                            # the duration of the poll
+	P_CODE2 = models.CharField(max_length=200, blank = True)               # the code2 of the poll (protocol number, approval number)
+	USER_CREATORID = models.ForeignKey(User, on_delete=models.CASCADE)     # the user (admin) who created the poll
+	UGRP_GROUPID = models.ForeignKey(Group, on_delete=models.CASCADE)      # the group of the user that created the poll
+	PCAT_CAT = models.ForeignKey(PollCat)                                  # the category of the poll
+	#PCHOICE_CHOICE = models.ForeignKey(PollChoice)                        # the voting choices of the poll
 	def __unicode__(self): 
 		return "%s %s" % (self.P_TITLE, self.P_BODY)
 
 
 class PollChoice(models.Model):
-	#PCHOICE_CHOICE = models.IntegerField(default=0)                       #the voting choices of the poll
+	#PCHOICE_CHOICE = models.IntegerField(default=0)                       # the voting choices of the poll
 	polls = models.ForeignKey(Polls, related_name='PChoice')
 	PCHOICE_CHOICE = models.CharField(max_length=100)
 	PCHOICE_VOTES = models.IntegerField(default=0)
--- Eisegesis/views.py
+++ Eisegesis/views.py
@@ -6,26 +6,26 @@ from django.urls import reverse
 from django.utils import timezone
 from django.contrib import messages
 
-from .models import Proposals,ProposalChoice,Voter
+from .models import Polls,PollChoice,Voter
 
 @login_required
 def index(request):
-	latest_proposal_list = Proposals.objects.order_by('-P_CREATION')#[:5]
+	latest_poll_list = Polls.objects.order_by('-P_CREATION')#[:5]
 	template = loader.get_template('Eisegesis/index.html')
 	context = {
-		'latest_proposal_list': latest_proposal_list,
+		'latest_poll_list': latest_poll_list,
 	}
 	return HttpResponse(template.render(context, request))
 
 
 @login_required
-def moreInfo(request, proposals_id):
-	proposal = get_object_or_404(Proposals, pk=proposals_id)
-	#PChoice = get_object_or_404(ProposalChoice)
-	template = loader.get_template('Eisegesis/proposal.html')
+def moreInfo(request, polls_id):
+	poll = get_object_or_404(Polls, pk=polls_id)
+	#PChoice = get_object_or_404(PollChoice)
+	template = loader.get_template('Eisegesis/poll.html')
 	nowDt = timezone.localtime(timezone.now())
 	context = {
-		'proposal': proposal,
+		'poll': poll,
 		'nowdt': nowDt,
 		#'PChoice': PChoice,
 	}
@@ -33,33 +33,33 @@ def moreInfo(request, proposals_id):
 	return HttpResponse(template.render(context, request))
 
 @login_required
-def vote(request, proposals_id):
-	proposal = get_object_or_404(Proposals, pk=proposals_id)
-	if Voter.objects.filter(Proposals_id=proposals_id, user_id=request.user.id).exists():
+def vote(request, polls_id):
+	poll = get_object_or_404(Polls, pk=polls_id)
+	if Voter.objects.filter(Polls_id=polls_id, user_id=request.user.id).exists():
 		messages.add_message(request, messages.INFO, 'You have already voted.')
-		return HttpResponseRedirect(reverse('Eisegesis:index'))#, args=(proposal.id,)))
+		return HttpResponseRedirect(reverse('Eisegesis:index'))#, args=(poll.id,)))
 	else:
 		try:
-			selected_choice = proposal.PChoice.get(pk=request.POST['choice'])
-		except (KeyError, ProposalChoice.DoesNotExist):						#probably wrong
+			selected_choice = poll.PChoice.get(pk=request.POST['choice'])
+		except (KeyError, PollChoice.DoesNotExist):						#probably wrong
 			# Redisplay the question voting form.
 			nowDt = timezone.localtime(timezone.now())
-			return render(request, 'Eisegesis/proposal.html', {
+			return render(request, 'Eisegesis/poll.html', {
 				#'question': question,
-				'proposal': proposal,
+				'poll': poll,
 				'nowdt': nowDt,
 				'error_message': "You didn't select a choice.",
 			})
 		else:
 			selected_choice.PCHOICE_VOTES += 1
 			selected_choice.save()
-			v = Voter(user=request.user, Proposals=proposal)
+			v = Voter(user=request.user, Polls=poll)
 			v.save()
 			# Always return an HttpResponseRedirect after successfully dealing
 			# with POST data. This prevents data from being posted twice if a
 			# user hits the Back button.
 			messages.add_message(request, messages.INFO, 'Vote submited successfully.')
-			return HttpResponseRedirect(reverse('Eisegesis:index'))#, args=(proposal.id,)))
+			return HttpResponseRedirect(reverse('Eisegesis:index'))#, args=(poll.id,)))
 
 #def index(request):
 #	return HttpResponse("Hello")
