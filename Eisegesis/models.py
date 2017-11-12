348c38466ff5e6cc652f237c722242fe05c4e9cd
panos panos <pantok3@gmail.com>
2017-11-12 16:08:31 +0200

    changed proposals related tables to polls

--- Eisegesis/models.py
+++ Eisegesis/models.py
@@ -6,33 +6,33 @@ from django.utils import timezone
 from django.contrib.auth.models import User, Group
 
 
-class ProposalCat(models.Model):
-	PCAT_CAT = models.CharField(max_length=100)                            #the category of the proposal
+class PollCat(models.Model):
+	PCAT_CAT = models.CharField(max_length=100)                            #the category of the poll
 	def __unicode__(self):
 		return self.PCAT_CAT
 
 
-class Proposals(models.Model):
-	P_CODE = models.CharField(max_length=200)                              #the code of the proposal
-	P_TITLE = models.CharField(max_length=100)                             #the title of the proposal
-	P_SHRBODY = models.CharField(max_length=300)                           #the short body of the proposal
-	P_BODY = models.CharField(max_length=1000)                             #the body (main text) of the proposal
-	P_CREATION = models.DateTimeField(default = timezone.now())            #the creation date and time of the proposal
-	P_STARTDT = models.DateTimeField()                                     #the starting date and time of the proposal
-	P_ENDDT = models.DateTimeField()                                       #the ending date and time of the proposal
-	P_DURATION = models.IntegerField(default=0)                            #the duration of the proposal
-	P_CODE2 = models.CharField(max_length=200, blank = True)               #the code2 of the proposal (protocol number, approval number)
-	USER_CREATORID = models.ForeignKey(User, on_delete=models.CASCADE)     #the user (admin) who created the proposal
-	UGRP_GROUPID = models.ForeignKey(Group, on_delete=models.CASCADE)      #the group of the user that created the proposal
-	PCAT_CAT = models.ForeignKey(ProposalCat)                              #the category of the proposal
-	#PCHOICE_CHOICE = models.ForeignKey(ProposalChoice)                    #the voting choices of the proposal
+class Polls(models.Model):
+	P_CODE = models.CharField(max_length=200)                              #the code of the poll
+	P_TITLE = models.CharField(max_length=100)                             #the title of the poll
+	P_SHRBODY = models.CharField(max_length=300)                           #the short body of the poll
+	P_BODY = models.CharField(max_length=1000)                             #the body (main text) of the poll
+	P_CREATION = models.DateTimeField(default = timezone.now())            #the creation date and time of the poll
+	P_STARTDT = models.DateTimeField()                                     #the starting date and time of the poll
+	P_ENDDT = models.DateTimeField()                                       #the ending date and time of the poll
+	P_DURATION = models.IntegerField(default=0)                            #the duration of the poll
+	P_CODE2 = models.CharField(max_length=200, blank = True)               #the code2 of the poll (protocol number, approval number)
+	USER_CREATORID = models.ForeignKey(User, on_delete=models.CASCADE)     #the user (admin) who created the poll
+	UGRP_GROUPID = models.ForeignKey(Group, on_delete=models.CASCADE)      #the group of the user that created the poll
+	PCAT_CAT = models.ForeignKey(PollCat)                              #the category of the poll
+	#PCHOICE_CHOICE = models.ForeignKey(PollChoice)                    #the voting choices of the poll
 	def __unicode__(self): 
 		return "%s %s" % (self.P_TITLE, self.P_BODY)
 
 
-class ProposalChoice(models.Model):
-	#PCHOICE_CHOICE = models.IntegerField(default=0)                       #the voting choices of the proposal
-	proposals = models.ForeignKey(Proposals, related_name='PChoice')
+class PollChoice(models.Model):
+	#PCHOICE_CHOICE = models.IntegerField(default=0)                       #the voting choices of the poll
+	polls = models.ForeignKey(Polls, related_name='PChoice')
 	PCHOICE_CHOICE = models.CharField(max_length=100)
 	PCHOICE_VOTES = models.IntegerField(default=0)
 	def __unicode__(self):
@@ -41,4 +41,4 @@ class ProposalChoice(models.Model):
 
 class Voter(models.Model):
 	user = models.ForeignKey(User)
-	Proposals = models.ForeignKey(Proposals)
+	Polls = models.ForeignKey(Polls)
