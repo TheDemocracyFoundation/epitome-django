38072ac723400ed5b40f88fd7cf141136a114fcc
panos panos <pantok3@gmail.com>
2017-12-16 12:21:09 +0200

    Comments removal
    Reason for change:
    * Python version 2 is not used.
    
    How does this change address the issue:
    * Source code clean up.
    
    Issue #32

--- Agora/models.py
+++ Agora/models.py
@@ -10,7 +10,7 @@ class VoteMain(models.Model):
     area = models.IntegerField(default=0)
     vote_proxy = models.IntegerField(default=0)
     pub_date = models.DateTimeField('date published')
-    def __str__(self):              # __unicode__ on Python 2
+    def __str__(self):
         return self.question_text
     def was_published_recently(self):
         now = timezone.now()
@@ -20,7 +20,7 @@ class VoteMain(models.Model):
 class VoteResult(models.Model):
     vote_main = models.OneToOneField(VoteMain, on_delete=models.DO_NOTHING)
     votes = models.IntegerField(default=0)
-    def __str__(self):              # __unicode__ on Python 2
+    def __str__(self):
         return self.votes
         
 
@@ -28,5 +28,5 @@ class VoteTexts(models.Model):
     vote_m = models.OneToOneField(VoteMain, on_delete=models.DO_NOTHING)
     vote_name = models.CharField(max_length=200)
     vote_description = models.CharField
-    def __str__(self):              # __unicode__ on Python 2
+    def __str__(self):
         return self.vote_name
