85dedfdaf73bd6ac8d7ac64262cf753d32b38e34
pbount <pbount01@dcs.bbk.ac.uk>
2018-04-29 12:00:45 +0100

    Working on Poll submission
    Reason for change:
    * Insufficient fields in poll creation template
    
    How does this change address the issue:
    * Added fields for Title, Short Body, Body, Start and End Date, Code and Category.
    * Modified user dropdown menu.
    * Removed sidebar (Replaced with a close button)

--- Demoscopesis/forms.py
+++ Demoscopesis/forms.py
@@ -5,8 +5,13 @@ from Demoscopesis.models import Poll
 class PollForm(ModelForm):
 	class Meta:
 		model = Poll
-		fields = ('PL_TITLE', 'PL_BODY')
+		fields = ('PL_TITLE', 'PL_SHRBODY', 'PL_BODY', 'PL_STARTDT', 'PL_ENDDT', 'PL_CODE', 'POLLCAT')
 		widgets = {
-			'PL_TITLE' : Textarea(attrs={'id':'Title','class':'form-control','rows':'10', 'required': True, 'autofocus':True}),
-			'PL_BODY' : forms.TextInput(attrs={'type':'text','id':'Body','class':'form-control mt-3','placeholder':'Body', 'required': True}),
+			'PL_TITLE' : forms.TextInput(attrs={'type':'text','id':'title','class':'form-control','placeholder':'Title', 'required': True}),
+			'PL_SHRBODY' : Textarea(attrs={'id':'shortbody','class':'form-control','rows':'3', 'required': True, 'autofocus':True}),
+			'PL_BODY' : Textarea(attrs={'id':'body','class':'form-control','rows':'10', 'required': True, 'autofocus':True}),
+			'PL_STARTDT' : forms.TextInput(attrs={'type':'date','id':'startdate','class':'form-control','required': True}),
+			'PL_ENDDT' : forms.TextInput(attrs={'type':'date','id':'enddate','class':'form-control','required': True}),
+			'PL_CODE' : forms.TextInput(attrs={'type':'text','id':'code','class':'form-control','required': True}),
+			'POLLCAT' : forms.TextInput(attrs={'type':'text','id':'code','class':'form-control','required': True}),
 		}
