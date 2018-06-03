dab49b4532d0d82771c2b920499664f325b4bb08
MavropaliasG <tefaa8@gmail.com>
2018-06-03 18:24:46 +0800

    autoinstaller asks for permission and also fixed bugs

--- AutoInstaller/Scripts/1autoinstaller.sh
+++ AutoInstaller/Scripts/1autoinstaller.sh
@@ -1,120 +1,179 @@
 #!/bin/bash
 
-echo 'Welcome to the Epitome AutoInstaller'
+echo 'Welcome to the Epitome AutoInstaller.'
 echo    # move to a new line
 
-# Remove previous Epitome installations
-rm -rf ~/Epitome/
+read -r -p "Would you like to install Epitome to your system? [y/n]  " response
+ response=${response,,} # tolower
+ echo    # move to a new line
+ if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
 
-rm -rf ~/tempepitome/
+    # Remove previous Epitome installations
+    rm -rf ~/Epitome/
 
-rm -rf ~/EpitomeVE
+    rm -rf ~/tempepitome/
 
-# Find out the installed package manager and customize the subsequent scripts
-if type apt 2> /dev/null; then
-   sudo apt-get install git python3 python3-venv python3-pip
+    rm -rf ~/EpitomeVE
+echo 'Detecting package manager...'
+    # Find out the installed package manager and customize the subsequent scripts
+    if type apt 2> /dev/null; then
+        echo    # move to a new line
+        sudo apt-get install git python3 python3-venv python3-pip
    
-   # Clone Epitome repository
-   mkdir ~/tempepitome/
-   cd ~/tempepitome/
+        # Clone Epitome repository
+        mkdir ~/tempepitome/
+        cd ~/tempepitome/
 
-   git clone https://github.com/TheDemocracyFoundation/epitome.git
+        git clone https://github.com/TheDemocracyFoundation/epitome.git
 
-   cd ~/tempepitome/epitome
+        cd ~/tempepitome/epitome
 
-   git checkout development
+        git checkout development
 
-   # Install pip and activate the python virtual environment
+        # Install pip and activate the python virtual environment
 
-   sudo pip install --upgrade pip setuptools
+        sudo pip install --upgrade pip setuptools
 
-   cd ~
+        cd ~
 
-   mkdir EpitomeVE
+        mkdir EpitomeVE
 
-   python3 -m venv EpitomeVE/
+        python3 -m venv EpitomeVE/
 
-   source ~/EpitomeVE/bin/activate
+        source ~/EpitomeVE/bin/activate
 
-   pip install --upgrade pip
+        pip install --upgrade pip
+        
+        # Install django and make migrations
+
+        pip install django
+
+        mkdir ~/Epitome/
+
+        cd ~/tempepitome/epitome
+
+        cp -rf * ~/Epitome
+
+        rm -rf ~/tempepitome/
+
+        cd ~/Epitome
+
+        symbols=({a..z} {A..Z} {0..9} '_' '-' '#' '$' '%' '^' '&' '*' '(' ')' '!' '@') length=50 key=; for (( i = 0; i < length; i++ )); do key+=${symbols[RANDOM%${#symbols[@]}]}; done; echo "$key" > ~/Epitome/secret_key.txt
+
+        python3 manage.py makemigrations Demoscopesis
+
+        python3 manage.py makemigrations Agora
+
+        python3 manage.py makemigrations Propylaea
+
+        python3 manage.py migrate
    
-elif type dnf 2> /dev/null; then
-   sudo dnf install git python3 python3-virtualenv python3-pip
+    elif type dnf 2> /dev/null; then
+        echo    # move to a new line
+        sudo dnf install git python3 python3-virtualenv python3-pip
    
-   # Clone Epitome repository
-   mkdir ~/tempepitome/
-   cd ~/tempepitome/
+        # Clone Epitome repository
+        mkdir ~/tempepitome/
+        cd ~/tempepitome/
 
-   git clone https://github.com/TheDemocracyFoundation/epitome.git
+        git clone https://github.com/TheDemocracyFoundation/epitome.git
 
-   cd ~/tempepitome/epitome
+        cd ~/tempepitome/epitome
 
-   git checkout development
+        git checkout development
 
-   # Install pip and activate the python virtual environment
+        # Install pip and activate the python virtual environment
 
-   sudo pip install --upgrade pip setuptools
+        sudo pip install --upgrade pip setuptools
 
-   cd ~
+        cd ~
 
-   mkdir EpitomeVE
+        mkdir EpitomeVE
 
-   python3 -m virtualenv EpitomeVE/
+        python3 -m virtualenv EpitomeVE/
 
-   source ~/EpitomeVE/bin/activate
+        source ~/EpitomeVE/bin/activate
 
-   pip install --upgrade pip
-elif type pacman 2> /dev/null; then
-   sudo pacman -S git python python-virtualenv python-pip
+        pip install --upgrade pip
+        
+
+        # Install django and make migrations
+
+        pip install django
+
+        mkdir ~/Epitome/
+
+        cd ~/tempepitome/epitome
+
+        cp -rf * ~/Epitome
+
+        rm -rf ~/tempepitome/
+
+        cd ~/Epitome
+
+        symbols=({a..z} {A..Z} {0..9} '_' '-' '#' '$' '%' '^' '&' '*' '(' ')' '!' '@') length=50 key=; for (( i = 0; i < length; i++ )); do key+=${symbols[RANDOM%${#symbols[@]}]}; done; echo "$key" > ~/Epitome/secret_key.txt
+
+        python3 manage.py makemigrations Demoscopesis
+
+        python3 manage.py makemigrations Agora
+
+        python3 manage.py makemigrations Propylaea
+
+        python3 manage.py migrate
+        
+    elif type pacman 2> /dev/null; then
+        echo    # move to a new line
+        sudo pacman -S git python python-virtualenv python-pip
       
-   # Clone Epitome repository
-   mkdir ~/tempepitome/
-   cd ~/tempepitome/
+        # Clone Epitome repository
+        mkdir ~/tempepitome/
+        cd ~/tempepitome/
 
-   git clone https://github.com/TheDemocracyFoundation/epitome.git
+        git clone https://github.com/TheDemocracyFoundation/epitome.git
 
-   cd ~/tempepitome/epitome
+        cd ~/tempepitome/epitome
 
-   git checkout development
+        git checkout development
 
-   # Install pip and activate the python virtual environment
+        # Install pip and activate the python virtual environment
 
-   sudo pip install --upgrade pip setuptools
+        sudo pip install --upgrade pip setuptools
 
-   cd ~
+        cd ~
 
-   mkdir EpitomeVE
+        mkdir EpitomeVE
 
-   python3 -m venv EpitomeVE/
+        python3 -m venv EpitomeVE/
 
-   source ~/EpitomeVE/bin/activate
+        source ~/EpitomeVE/bin/activate
 
-   pip install --upgrade pip
-else
-   echo "Supported package manager not found (apt, dnf, pacman)" >&2
-   exit 1
-fi
+        pip install --upgrade pip
+        
+        # Install django and make migrations
 
-# Install django and make migrations
+        pip install django
 
-pip install django
+        mkdir ~/Epitome/
 
-mkdir ~/Epitome/
+        cd ~/tempepitome/epitome
 
-cd ~/tempepitome/epitome
+        cp -rf * ~/Epitome
 
-cp -rf * ~/Epitome
+        rm -rf ~/tempepitome/
 
-rm -rf ~/tempepitome/
+        cd ~/Epitome
 
-cd ~/Epitome
+        symbols=({a..z} {A..Z} {0..9} '_' '-' '#' '$' '%' '^' '&' '*' '(' ')' '!' '@') length=50 key=; for (( i = 0; i < length; i++ )); do key+=${symbols[RANDOM%${#symbols[@]}]}; done; echo "$key" > ~/Epitome/secret_key.txt
 
-symbols=({a..z} {A..Z} {0..9} '_' '-' '#' '$' '%' '^' '&' '*' '(' ')' '!' '@') length=50 key=; for (( i = 0; i < length; i++ )); do key+=${symbols[RANDOM%${#symbols[@]}]}; done; echo "$key" > ~/Epitome/secret_key.txt
+        python3 manage.py makemigrations Demoscopesis
 
-python3 manage.py makemigrations Demoscopesis
+        python3 manage.py makemigrations Agora
 
-python3 manage.py makemigrations Agora
+        python3 manage.py makemigrations Propylaea
 
-python3 manage.py makemigrations Propylaea
-
-python3 manage.py migrate
+        python3 manage.py migrate
+    else
+        echo "Supported package manager not found (apt, dnf, pacman)" >&2
+        exit 1
+    fi
+ fi
