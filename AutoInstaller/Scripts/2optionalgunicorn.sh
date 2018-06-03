85668e5caca0a9590b5f17022f42e7036d5e5e45
Georgios Mavropalias <MavropaliasG@users.noreply.github.com>
2018-06-03 14:05:46 +0800

    Update 2optionalgunicorn.sh

--- AutoInstaller/Scripts/2optionalgunicorn.sh
+++ AutoInstaller/Scripts/2optionalgunicorn.sh
@@ -10,4 +10,6 @@ EXTERNALIP="$(curl ipinfo.io/ip)"
 
 sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = ['$EXTERNALIP','localhost']/" ~/Epitome/Epitome/settings.py
 
+sed -i "s/DEBUG = True/DEBUG = False/" ~/Epitome/Epitome/settings.py
+
 gunicorn --bind $EXTERNALIP:8000 Epitome.wsgi
