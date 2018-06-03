6aab30850377b783d374450f7025c18f98408848
Georgios Mavropalias <MavropaliasG@users.noreply.github.com>
2018-06-03 13:54:11 +0800

    Update settings.py

--- Epitome/settings.py
+++ Epitome/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-f = open('key.txt') 
+f = open('secret_key.txt') 
 
 SECRET_KEY = f.read()
 
