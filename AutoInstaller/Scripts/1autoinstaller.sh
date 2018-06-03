0432034664702884c5fccc4b9d8d9df07ef8f328
MavropaliasG <tefaa8@gmail.com>
2018-06-03 18:43:36 +0800

    autoinstaller includes license in the preamble

--- AutoInstaller/Scripts/1autoinstaller.sh
+++ AutoInstaller/Scripts/1autoinstaller.sh
@@ -3,6 +3,26 @@
 echo 'Welcome to the Epitome AutoInstaller.'
 echo    # move to a new line
 
+
+echo 'Epitome'
+echo 'Copyright (C) 2015 The Democracy Foundation'
+echo 'http://democracy.foundation/'
+echo    # move to a new line
+echo 'This program is free software: you can redistribute it and/or modify'
+echo 'it under the terms of the GNU General Public License as published by'
+echo 'the Free Software Foundation, either version 3 of the License, or'
+echo '(at your option) any later version.'
+echo    # move to a new line
+echo 'This program is distributed in the hope that it will be useful,'
+echo 'but WITHOUT ANY WARRANTY; without even the implied warranty of'
+echo 'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
+echo 'GNU General Public License for more details.'
+echo    # move to a new line
+echo 'You should have received a copy of the GNU General Public License'
+echo 'along with this program.  If not, see <https://www.gnu.org/licenses/>.'
+echo    # move to a new line
+echo    # move to a new line
+
 read -r -p "Would you like to install Epitome to your system? [y/n]  " response
  response=${response,,} # tolower
  echo    # move to a new line
