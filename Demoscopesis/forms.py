ae4efa719013c46b0bbedf4485ce5ea18cfa235c
pbount <pbount01@dcs.bbk.ac.uk>
2018-06-22 17:44:46 +0100

    Uncommitted partial files
    Reason for change:
    * ...
    
    How does this change address the issue:
    * ...

--- Demoscopesis/forms.py
+++ Demoscopesis/forms.py
@@ -7,12 +7,12 @@ class PollForm(ModelForm):
 		model = Poll
 		fields = ('PL_TITLE', 'PL_SHRBODY', 'PL_BODY', 'PL_STARTDT', 'PL_ENDDT', 'PL_CODE', 'POLLCAT')
 		widgets = {
+			'PL_CODE' : forms.TextInput(attrs={'type':'text','id':'code','class':'form-control','required': True}),
 			'PL_TITLE' : forms.TextInput(attrs={'type':'text','id':'title','class':'form-control','required': True}),
 			'PL_SHRBODY' : Textarea(attrs={'id':'shortbody','class':'form-control','rows':'3', 'required': True, 'autofocus':True}),
 			'PL_BODY' : Textarea(attrs={'id':'body','class':'form-control','rows':'10', 'required': True, 'autofocus':True}),
 			'PL_STARTDT' : forms.TextInput(attrs={'type':'date','id':'startdate','class':'form-control','required': True}),
 			'PL_ENDDT' : forms.TextInput(attrs={'type':'date','id':'enddate','class':'form-control','required': True}),
-			'PL_CODE' : forms.TextInput(attrs={'type':'text','id':'code','class':'form-control','required': True}),
 			'POLLCAT' : forms.TextInput(attrs={'type':'text','id':'category','class':'form-control','required': True})
 		}
 
