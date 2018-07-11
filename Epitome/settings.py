0a7c5d964429af5ae53b9b2731604133f8d6c2cc
MavropaliasG <tefaa8@gmail.com>
2018-07-12 02:05:42 +0800

    changes for heroku

--- Epitome/settings.py
+++ Epitome/settings.py
@@ -134,3 +134,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+# ie if Heroku server
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
