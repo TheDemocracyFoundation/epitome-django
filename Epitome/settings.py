8aa28f7ad1bbbb752de8b9c555433bce22ee3061
MavropaliasG <tefaa8@gmail.com>
2018-07-12 01:54:56 +0800

    changes for heroku

--- Epitome/settings.py
+++ Epitome/settings.py
@@ -130,7 +130,7 @@ SESSION_COOKIE_AGE = 600
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
