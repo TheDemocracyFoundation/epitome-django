a291ba0f7ab6f5a7185283374e228bdaa02373ea
Georgios Mavropalias <MavropaliasG@users.noreply.github.com>
2018-07-12 01:33:41 +0800

    Update settings.py

--- Epitome/settings.py
+++ Epitome/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.environ.get('EPITOME_SECRET_KEY')
-s
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
