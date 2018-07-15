1d61764c07647476465eb24a37943ff200f93e79
MavropaliasG <tefaa8@gmail.com>
2018-07-15 21:21:00 +0800

    trying to automatically generate secret key in heroku

--- Epitome/settings.py
+++ Epitome/settings.py
@@ -23,8 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('EPITOME_SECRET_KEY')
+# SECRET_KEY = os.environ.get('EPITOME_SECRET_KEY')
 
+SECRET_KEY = ergdfgdfgsfdsgd
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
