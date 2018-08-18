c2c3ed9cbea5fcb2ebbf5c12bf32a77c42d54079
Mr Pavlos Bountagkidis <pbount01@dcs.bbk.ac.uk>
2018-08-18 17:50:47 +0100

    Removed unesessary html elements

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
@@ -5,8 +7,12 @@ from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.contrib import messages
 
+from Demoscopesis.models import Poll
+
 
 @login_required(login_url='/user/login/')
 def index(request):
     template = loader.get_template('Dashboard/home.html')
-    return HttpResponse(template.render({},request))
+    request.activeCount = Poll.objects.filter(PL_ENDDT__gt=datetime.now()).count()
+    request.inactiveCount = Poll.objects.filter(PL_ENDDT__lt=datetime.now()).count()
+    return HttpResponse(template.render({}, request))
