16c5117631ff42d02a80e4ee66d73f24e92e9ec8
Mr Pavlos Bountagkidis <pbount01@dcs.bbk.ac.uk>
2018-08-25 14:05:37 +0100

    Modified autofocus attribute in Polls model
    Reason for change:
    ! As Identified by MavropaliasG: Opening the new poll creation
      page enables the second text field instead of the title text field.
    * Erroneous attribute in models PL_SHRBODY and PL_BODY caused the
      issue.
    
    How does this change address the issue:
    * Moved the attribute to the appropriate field.

--- Demoscopesis/forms.py
+++ Demoscopesis/forms.py
@@ -8,9 +8,9 @@ class PollForm(ModelForm):
 		fields = ('PL_TITLE', 'PL_SHRBODY', 'PL_BODY', 'PL_STARTDT', 'PL_ENDDT', 'PL_CODE', 'POLLCAT')
 		widgets = {
 			'PL_CODE' : forms.TextInput(attrs={'type':'text','id':'code','class':'form-control','required': True}),
-			'PL_TITLE' : forms.TextInput(attrs={'type':'text','id':'title','class':'form-control','required': True}),
-			'PL_SHRBODY' : Textarea(attrs={'id':'shortbody','class':'form-control','rows':'3', 'required': True, 'autofocus':True}),
-			'PL_BODY' : Textarea(attrs={'id':'body','class':'form-control','rows':'10', 'required': True, 'autofocus':True}),
+			'PL_TITLE' : forms.TextInput(attrs={'type':'text','id':'title','class':'form-control','required': True, 'autofocus':True}),
+			'PL_SHRBODY' : Textarea(attrs={'id':'shortbody','class':'form-control','rows':'3', 'required': True}),
+			'PL_BODY' : Textarea(attrs={'id':'body','class':'form-control','rows':'10', 'required': True}),
 			'PL_STARTDT' : forms.TextInput(attrs={'type':'date','id':'startdate','class':'form-control','required': True}),
 			'PL_ENDDT' : forms.TextInput(attrs={'type':'date','id':'enddate','class':'form-control','required': True}),
 			'PL_STIME' : forms.TextInput(attrs={'type':'text','id':'stime','class':'form-control','required': True}),
