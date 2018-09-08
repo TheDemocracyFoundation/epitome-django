52996fbfd2df2619d6336d481714a590e5478ad7
Georgios Mavropalias <MavropaliasG@users.noreply.github.com>
2018-09-08 23:55:37 +0800

    Update settings.py

--- Epitome/settings.py
+++ Epitome/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('EPITOME_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', '.herokuapp.com']
 
