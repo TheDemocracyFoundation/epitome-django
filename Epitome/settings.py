3ecff3f7f5fdf7ef891970e2ff6fbee0617118ca
Georgios Mavropalias <MavropaliasG@users.noreply.github.com>
2018-09-09 00:31:38 +0800

    Update settings.py

--- Epitome/settings.py
+++ Epitome/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('EPITOME_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', '.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
