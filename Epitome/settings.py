2f4703d0b844dc277420ebf2ca4c412a96a8b31d
Georgios Mavropalias <MavropaliasG@users.noreply.github.com>
2018-09-08 21:10:18 +0800

    Update settings.py

--- Epitome/settings.py
+++ Epitome/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+	'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
