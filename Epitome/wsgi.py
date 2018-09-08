c9f719790894541976ade95835bba669ccc989b0
Georgios Mavropalias <MavropaliasG@users.noreply.github.com>
2018-09-08 21:08:32 +0800

    Update wsgi.py

--- Epitome/wsgi.py
+++ Epitome/wsgi.py
@@ -17,7 +17,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Epitome.settings")
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
 
 application = MyWSGIApp()
 application = WhiteNoise(application, root='/static/')
