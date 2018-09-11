1472eeaa5bfde5745057e87ad748a0d5a770c7b7
Georgios Mavropalias <MavropaliasG@users.noreply.github.com>
2018-09-12 01:42:23 +0800

    Update epitomeupdate.sh

--- AutoInstaller/Scripts/epitomeupdate.sh
+++ AutoInstaller/Scripts/epitomeupdate.sh
@@ -19,9 +19,25 @@ read -r -p "Are you sure you want to update Epitome? This will not replace your 
 
     source ~/EpitomeVE/bin/activate
     
-    pip3 install --upgrade pip
-
-    pip3 install django whitenoise
+echo 'Updating pip and required packages'
+echo    # move to a new line
+    if type apt 2> /dev/null; then
+        sudo pip3 install --upgrade pip setuptools
+        pip3 install --upgrade pip
+        pip3 install django whitenoise
+    elif type dnf 2> /dev/null; then
+        sudo pip install --upgrade pip setuptools
+        pip install --upgrade pip
+        pip install django whitenoise
+    elif type yum 2> /dev/null; then
+        sudo pip3 install --upgrade pip setuptools
+        pip install --upgrade pip
+        pip install django whitenoise
+    elif type pacman 2> /dev/null; then
+        sudo pip install --upgrade pip setuptools
+        pip install --upgrade pip
+        pip install django whitenoise
+    fi
 
     echo    # move to a new line
     read -r -p "Would you like to replace your current settings.py? (this will delete your current configuration) [y/n]  " response
