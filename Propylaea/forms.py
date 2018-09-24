981148413edb30ccabba367abda5c3a70aa460ee
Pavlos <pavlos.boudagidis@gmail.com>
2018-09-24 16:10:24 +0100

    Forms fix.
    Reason for change:
    * .
    
    How does this change address the issue:
    * minor change

--- Propylaea/forms.py
+++ Propylaea/forms.py
@@ -3,22 +3,33 @@ from django.forms import ModelForm
 from django.contrib.auth.models import User
 
 
-
 class LoginForm(ModelForm):
     class Meta:
         model = User
         fields = ('username', 'password')
         widgets = {
-			'username' : forms.TextInput(attrs={'type':'text','id':'login-username', 'class':'form-control','placeholder':'Username','name': 'username', 'required': True, 'autofocus':True}),
-			'password' : forms.PasswordInput(attrs={'type':'password','id':'login-password','class':'form-control mt-3','placeholder':'Password','max_length': '64', 'name': 'password', 'required': True})
+            'username': forms.TextInput(
+                attrs={'type': 'text', 'id': 'login-username', 'class': 'form-control', 'placeholder': 'Username',
+                       'name': 'username', 'required': True, 'autofocus': True}),
+            'password': forms.PasswordInput(
+                attrs={'type': 'password', 'id': 'login-password', 'class': 'form-control mt-3',
+                       'placeholder': 'Password', 'max_length': '64', 'name': 'password', 'required': True})
         }
-        
+
+
 class SignUpForm(ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
         widgets = {
-			'username' : forms.TextInput(attrs={'type':'text','id':'registration-username','class':'form-control','placeholder':'Username','name': 'username', 'required': True, 'autofocus':True, 'maxlength':40}),
-			'email' : forms.EmailInput(attrs={'type':'text','id':'registration-email','class':'form-control mt-3','placeholder':'e-mail','name': 'email', 'required': True}),
-			'password' : forms.PasswordInput(attrs={'type':'password','id':'registration-password','class':'form-control mt-3','placeholder':'Password','name': 'password', 'required': True, 'data-toggle':'popover', 'data-placement':'right', 'data-content':'Length must be over 8 characters long', 'data-trigger':'manual'}),
-       }
+            'username': forms.TextInput(attrs={'type': 'text', 'id': 'registration-username', 'class': 'form-control',
+                                               'placeholder': 'Username', 'name': 'username', 'required': True,
+                                               'autofocus': True}),
+            'email': forms.EmailInput(attrs={'type': 'text', 'id': 'registration-email', 'class': 'form-control mt-3',
+                                             'placeholder': 'e-mail', 'name': 'email', 'required': True}),
+            'password': forms.PasswordInput(
+                attrs={'type': 'password', 'id': 'registration-password', 'class': 'form-control mt-3',
+                       'placeholder': 'Password', 'name': 'password', 'required': True, 'data-toggle': 'popover',
+                       'data-placement': 'right', 'data-content': 'Length must be over 8 characters long',
+                       'data-trigger': 'manual'}),
+        }
